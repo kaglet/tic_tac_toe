77f6a8636e1b13137ac1026e969936bdd8383e27
Kago <kmotlhabane769@gmail.com>
2024-01-13 11:34:26 +0200

    Create gameboard entity with initialized board property

---
+++ script.js
@@ -0,0 +1,13 @@
+const gameboard = function () {
+    const cols = rows = 3;
+    board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < cols; j++) {
+            board[i].push("cell");            
+        }
+    }
+
+    console.dir(board);
+}();
